230f4a8ad5c6dfd220eda35c3a74ae0bf57ac083
AbandonedDemon <hoanghiepgamer@gmail.com>
2025-01-24 23:19:42 +0700

    Added missing repository implementations; Set up UnitOfWork

--- BE/SkincareBookingSystem.DataAccess/IRepositories/IUnitOfWork.cs
+++ BE/SkincareBookingSystem.DataAccess/IRepositories/IUnitOfWork.cs
@@ -2,5 +2,29 @@
 
 public interface IUnitOfWork
 {
+    IAppointmentsRepository Appointments { get; }
+    IBlogRepository Blog { get; }
+    IBlogCategoryRepository BlogCategory { get; }
+    IComboItemRepository ComboItem { get; }
+    ICustomerRepository Customer { get; }
+    ICustomerSkinTestRepository CustomerSkinTest { get; }
+    IDurationItemRepository DurationItem { get; }
+    IFeedbacksRepository Feedbacks { get; }
+    IOrderRepository Order { get; }
+    IOrderDetailRepository OrderDetail { get; }
+    IServiceComboRepository ServiceCombo { get; }
+    IServiceDurationRepository ServiceDuration { get; }
+    IServicesRepository Services { get; }
+    IServiceTypeRepository ServiceType { get; }
+    ISkinProfileRepository SkinProfile { get; }
+    ISkinServiceTypeRepository SkinServiceType { get; }
+    ISkinTestRepository SkinTest { get; }
+    ISkinTherapistRepository SkinTherapist { get; }
+    ISlotRepository Slot { get; }
+    ITestAnswerRepository TestAnswer { get; }
+    ITestQuestionRepository TestQuestion { get; }
+    ITherapistScheduleRepository TherapistSchedule { get; }
+    ITypeItemRepository TypeItem { get; }
+    IUserManagerRepository UserManager { get; }
     Task<int> SaveAsync();
 }
