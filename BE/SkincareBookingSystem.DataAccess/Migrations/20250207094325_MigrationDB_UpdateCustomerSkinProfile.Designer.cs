d67602a0413d2330b7cc9012d0e0d430ef61cb94
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-02-08 17:35:58 +0700

    modify DB migration; Nullable skinprofileId, therapistscheduleId

--- BE/SkincareBookingSystem.DataAccess/Migrations/20250207094325_MigrationDB_UpdateCustomerSkinProfile.Designer.cs
+++ BE/SkincareBookingSystem.DataAccess/Migrations/20250207094325_MigrationDB_UpdateCustomerSkinProfile.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SkincareBookingSystem.DataAccess.DBContext;
@@ -11,9 +12,11 @@ using SkincareBookingSystem.DataAccess.DBContext;
 namespace SkincareBookingSystem.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250207094325_MigrationDB_UpdateCustomerSkinProfile")]
+    partial class MigrationDB_UpdateCustomerSkinProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -807,7 +810,7 @@ namespace SkincareBookingSystem.DataAccess.Migrations
                         .HasMaxLength(20)
                         .HasColumnType("character varying(20)");
 
-                    b.Property<Guid?>("TherapistScheduleId")
+                    b.Property<Guid>("TherapistScheduleId")
                         .HasColumnType("uuid");
 
                     b.Property<string>("UserId")
@@ -1207,7 +1210,9 @@ namespace SkincareBookingSystem.DataAccess.Migrations
                 {
                     b.HasOne("SkincareBookingSystem.Models.Domain.TherapistSchedule", "TherapistSchedule")
                         .WithMany()
-                        .HasForeignKey("TherapistScheduleId");
+                        .HasForeignKey("TherapistScheduleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.HasOne("SkincareBookingSystem.Models.Domain.ApplicationUser", "ApplicationUser")
                         .WithMany()
