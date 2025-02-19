dd12e61e67966345d00673d02b4e9810cc35756a
AbandonedDemon <hoanghiepgamer@gmail.com>
2025-02-19 23:17:51 +0700

    Helper methods & constants to generate ResponseDto's with pre-defined messages

--- BE/SkincareBookingSystem.Utilities/Constants/StaticOperationStatus.cs
+++ BE/SkincareBookingSystem.Utilities/Constants/StaticOperationStatus.cs
@@ -9,6 +9,18 @@ namespace SkincareBookingSystem.Utilities.Constants
     public static class StaticOperationStatus
     {
         /// <summary>
+        /// Constants related to appointment statuses.
+        /// </summary>
+        public static class Appointment
+        {
+            public const string Created = "CREATED";
+            public const string Confirmed = "CONFIRMED";
+            public const string Cancelled = "CANCELLED";
+            public const string Completed = "COMPLETED";
+            public const string Rescheduled = "RESCHEDULED";
+            public const string Deleted = "DELETED";
+        }
+        /// <summary>
         /// Constants related to BaseEntity statuses.
         /// </summary>
         public static class BaseEntity
@@ -23,6 +35,27 @@ namespace SkincareBookingSystem.Utilities.Constants
         {
             public const int Success = 1;
             public const int Failure = 0;
+        }
+
+        public static class StatusCode
+        {
+            public const int Ok = 200;
+            public const int Created = 201;
+            public const int NoContent = 204;
+            public const int BadRequest = 400;
+            public const int Unauthorized = 401;
+            public const int Forbidden = 403;
+            public const int NotFound = 404;
+            public const int InternalServerError = 500;
+        }
+
+
+        /// <summary>
+        /// Constants related to timezones (primarily Vietnam).
+        /// </summary>
+        public static class Timezone
+        {
+            public static readonly DateTime Vietnam = DateTime.UtcNow.AddHours(7.0);
         }
     }
 }
