ea19603ad9ce6492d3c7204bea670f875ae4d2cf
LongTran3004 <quylongtranngoc07@gmail.com>
2025-02-20 15:33:02 +0700

    Fixed AutoMapper for Update ServicesService

--- BE/SkincareBookingSystem.Services/Mapping/AutoMapperProfile.cs
+++ BE/SkincareBookingSystem.Services/Mapping/AutoMapperProfile.cs
@@ -19,6 +19,11 @@ public class AutoMapperProfile : Profile
 
         CreateMap<UpdateServiceDto, Models.Domain.Services>()
             .ForMember(dest => dest.UpdatedTime, opt => opt.MapFrom(src => DateTime.UtcNow.AddHours(7.0))) //Meaning of 7.0 is GMT+7
-            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null)); // Ignore null value
+            .ForMember(dest => dest.ServiceName, opt => opt.MapFrom(src => src.ServiceName))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+            .ForMember(dest => dest.ServiceTypeId, opt => opt.Condition((src =>
+                   src.ServiceTypeId.HasValue && src.ServiceTypeId.Value != Guid.Empty)));
     }
 }
