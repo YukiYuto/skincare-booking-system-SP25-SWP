8fd7618369de10db22dbc7fc651c66f747aa2f63
AbandonedDemon <hoanghiepgamer@gmail.com>
2025-02-26 20:59:20 +0700

    Implementation for Customer & Skin Therapist information views.

---
+++ BE/SkincareBookingSystem.API/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkincareBookingSystem.Services.IServices;
+
+namespace SkincareBookingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            var response = await _customerService.GetAllCustomers();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetCustomerDetailsById(Guid customerId)
+        {
+            var response = await _customerService.GetCustomerDetailsById(User, customerId);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
---
+++ BE/SkincareBookingSystem.Services/IServices/ISkinTherapistService.cs
@@ -0,0 +1,15 @@
+﻿using SkincareBookingSystem.Models.Dto.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkincareBookingSystem.Services.IServices
+{
+    public interface ISkinTherapistService
+    {
+        Task<ResponseDto> GetTherapistDetailsById(Guid therapistId);
+        Task<ResponseDto> GetAllTherapists();
+    }
+}
