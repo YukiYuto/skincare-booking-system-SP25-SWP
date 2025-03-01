79c2fe927bb66134a44bffc4005bfc9823cbfb31
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-03-01 10:23:25 +0700

    MigrationDb TherapistServiceType add BaseEntityr

---
+++ BE/SkincareBookingSystem.DataAccess/Migrations/20250301032023_MigrationDB_TherapistServiceType-BaseEntity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkincareBookingSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDB_TherapistServiceTypeBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "TherapistServiceTypes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedTime",
+                table: "TherapistServiceTypes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "TherapistServiceTypes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "TherapistServiceTypes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedTime",
+                table: "TherapistServiceTypes",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "TherapistServiceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedTime",
+                table: "TherapistServiceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "TherapistServiceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "TherapistServiceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedTime",
+                table: "TherapistServiceTypes");
+        }
+    }
+}
