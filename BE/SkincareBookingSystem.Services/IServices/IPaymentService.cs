586549b1462e76f798080862ee3440ee160eef99
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-03-05 12:02:04 +0700

    fix claims in GetAll Payment

--- BE/SkincareBookingSystem.Services/IServices/IPaymentService.cs
+++ BE/SkincareBookingSystem.Services/IServices/IPaymentService.cs
@@ -6,8 +6,18 @@ namespace SkincareBookingSystem.Services.IServices;
 
 public interface IPaymentService
 {
-
     Task<ResponseDto> CreatePayOsPaymentLink(ClaimsPrincipal User, CreatePaymentLinkDto createPaymentLinkDto);
     Task<ResponseDto> ConfirmPayOsTransaction(ConfirmPaymentDto confirmPaymentDto);
-    Task<ResponseDto> CancelPayOsPaymentLink(ClaimsPrincipal User, Guid paymentTransactionId, string cancellationReason);
+
+    Task<ResponseDto> GetAll
+    (
+        ClaimsPrincipal User,
+        int pageNumber = 1,
+        int pageSize = 10,
+        string? filterOn = null,
+        string? filterQuery = null,
+        string? sortBy = null
+    );
+
+    Task<ResponseDto> GetPaymentById(ClaimsPrincipal User, Guid paymentTransactionId);
 }
