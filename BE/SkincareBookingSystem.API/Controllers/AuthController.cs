5aedf490db2c4dd0001cd984aa2254e541a9967a
LongTran3004 <quylongtranngoc07@gmail.com>
2025-03-06 16:28:25 +0700

    Added API document for AuthController

--- BE/SkincareBookingSystem.API/Controllers/AuthController.cs
+++ BE/SkincareBookingSystem.API/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using SkincareBookingSystem.Models.Dto.Response;
 using SkincareBookingSystem.Services.IServices;
 using System.Security.Claims;
 using SkincareBookingSystem.Models.Dto.Email;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace SkincareBookingSystem.API.Controllers;
 
@@ -28,6 +29,7 @@ public class AuthController : ControllerBase
     /// <param name="signUpCustomerDto"></param>
     /// <returns></returns>
     [HttpPost("customers")]
+    [SwaggerOperation(Summary = "API đăng ký người dùng mới")]
     public async Task<ActionResult<ResponseDto>> SignUpCustomer([FromBody] SignUpCustomerDto signUpCustomerDto)
     {
         if (!ModelState.IsValid)
@@ -50,6 +52,7 @@ public class AuthController : ControllerBase
     /// <param name="SignUpSkinTherapistDto"></param>
     /// <returns></returns>
     [HttpPost("skin-therapists")]
+    [SwaggerOperation(Summary = "API đăng ký SkinTherapist mới")]
     public async Task<ActionResult<ResponseDto>> SignUpSkinTherapist(
         [FromBody] SignUpSkinTherapistDto signUpSkinTherapistDto)
     {
@@ -73,6 +76,7 @@ public class AuthController : ControllerBase
     /// <param name="signUpStaffDto"></param>
     /// <returns></returns>
     [HttpPost("staff")]
+    [SwaggerOperation(Summary = "API đăng ký Staff mới")]
     public async Task<ActionResult<ResponseDto>> SignUpStaff([FromBody] SignUpStaffDto signUpStaffDto)
     {
         if (!ModelState.IsValid)
@@ -95,6 +99,7 @@ public class AuthController : ControllerBase
     /// <param name="signUpCustomerDto"></param>
     /// <returns></returns>
     [HttpPost("sign-in")]
+    [SwaggerOperation(Summary = "API đăng nhập người dùng")]
     public async Task<ActionResult<ResponseDto>> SignIn([FromBody] SignInDto signInDto)
     {
         var responseDto = await _authService.SignIn(signInDto);
@@ -107,6 +112,7 @@ public class AuthController : ControllerBase
     /// <param name="token"></param>
     /// <returns></returns>
     [HttpGet("FetchUserByToken")]
+    [SwaggerOperation(Summary = "API lấy thông tin người dùng bằng token")]
     public async Task<IActionResult> FetchUserByToken(string token)
     {
         var responseDto = await _authService.FetchUserByToken(token);
@@ -119,6 +125,7 @@ public class AuthController : ControllerBase
     /// <param name="changePasswordDto"></param>
     /// <returns></returns>
     [HttpPost("ChangePassword")]
+    [SwaggerOperation(Summary = "API thay đổi mật khẩu")]
     public async Task<IActionResult> ChangePassword(ChangePasswordDto changePasswordDto)
     {
         // Lấy Id người dùng hiện tại.
@@ -142,6 +149,7 @@ public class AuthController : ControllerBase
     /// <param name="updateUserProfileDto"></param>
     /// <returns></returns>
     [HttpPost("UpdateUserProfile")]
+    [SwaggerOperation(Summary = "API cập nhật thông tin người dùng")]
     public async Task<IActionResult> UpdateUserProfile(UpdateUserProfileDto updateUserProfileDto)
     {
         var responseDto = await _authService.UpdateUserProfile(User, updateUserProfileDto);
@@ -155,6 +163,7 @@ public class AuthController : ControllerBase
     /// <param name="emailDto"></param>
     /// <returns></returns>
     [HttpPost("send-verify-email")]
+    [SwaggerOperation(Summary = "API gửi email xác thực")]
     public async Task<IActionResult> SendVerifyEmail([FromBody] EmailDto emailDto)
     {
         var responseDto = await _authService.SendVerifyEmail(emailDto);
@@ -167,6 +176,7 @@ public class AuthController : ControllerBase
     /// <param name="verifyEmailDto"></param>
     /// <returns></returns>
     [HttpPost("verify-email")]
+    [SwaggerOperation(Summary = "API xác thực email")]
     public async Task<IActionResult> VerifyEmail([FromBody] VerifyEmailDto verifyEmailDto)
     {
         var responseDto = await _authService.VerifyEmail(verifyEmailDto);
@@ -174,6 +184,7 @@ public class AuthController : ControllerBase
     }
     
     [HttpPost("forgot-password")]
+    [SwaggerOperation(Summary = "API quên mật khẩu")]
     public async Task<IActionResult> ForgotPassword([FromBody] EmailDto forgotPasswordDto)
     {
         var responseDto = await _authService.ForgotPassword(forgotPasswordDto);
@@ -181,6 +192,7 @@ public class AuthController : ControllerBase
     }
     
     [HttpPost("reset-password")]
+    [SwaggerOperation(Summary = "API reset mật khẩu")]
     public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
     {
         var responseDto = await _authService.ResetPassword(resetPasswordDto);
