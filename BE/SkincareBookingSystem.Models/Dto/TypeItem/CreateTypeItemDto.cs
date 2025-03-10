6b2559eacf910a0653509a19984076aa692b61ff
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-03-10 20:54:36 +0700

    Push all

---
+++ BE/SkincareBookingSystem.Models/Dto/TypeItem/CreateTypeItemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SkincareBookingSystem.Models.Dto.TypeItem;
+
+public class CreateTypeItemDto 
+{
+    public Guid ServiceId { get; set; }
+    public List<Guid> ServiceTypeIdList { get; set; }
+}
