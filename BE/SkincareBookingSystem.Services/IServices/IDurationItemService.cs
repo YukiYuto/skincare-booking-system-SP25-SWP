94938249243364b58670aef7f42d4a708dfd466d
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-03-13 13:27:22 +0700

    Get all ServiceDuration and fix fetchUser

---
+++ BE/SkincareBookingSystem.Services/IServices/IDurationItemService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using SkincareBookingSystem.Models.Dto.DurationItem;
+using SkincareBookingSystem.Models.Dto.Response;
+
+namespace SkincareBookingSystem.Services.IServices;
+
+public interface IDurationItemService
+{
+    Task<ResponseDto> AddDurationItemAsync(ClaimsPrincipal User, CreateDurationItemDto durationItemDto);
+    Task<ResponseDto> DeleteDurationItemAsync(Guid id);
+}
