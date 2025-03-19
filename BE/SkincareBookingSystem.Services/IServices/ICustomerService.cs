d163c01baaed9e021306e1d5974c42bcd1720473
PhamQuan2003 <bestzed12345ff@gmail.com>
2025-03-19 19:13:28 +0700

    Get all customer, appointment and GetCustomerTimeTable

--- BE/SkincareBookingSystem.Services/IServices/ICustomerService.cs
+++ BE/SkincareBookingSystem.Services/IServices/ICustomerService.cs
@@ -13,7 +13,6 @@ namespace SkincareBookingSystem.Services.IServices
         Task<ResponseDto> GetCustomerDetailsById(ClaimsPrincipal user, Guid customerId);
         Task<ResponseDto> GetAllCustomers();
         Task<ResponseDto> GetCustomerIdByUserId(ClaimsPrincipal User);
-        Task<ResponseDto> GetCustomerInfoByEmailAsync(string email);
-        Task<ResponseDto> GetCustomerInfoByPhoneNumberAsync(string phoneNumber);
+        Task<ResponseDto> GetCustomerTimeTable(ClaimsPrincipal User);
     }
 }
