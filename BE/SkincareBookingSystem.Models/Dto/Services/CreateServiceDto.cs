2c1b310e77ef5f48cf8723e515e62553e089342d
LongTran3004 <quylongtranngoc07@gmail.com>
2025-03-29 18:00:00 +0700

    Changed income price value

--- BE/SkincareBookingSystem.Models/Dto/Services/CreateServiceDto.cs
+++ BE/SkincareBookingSystem.Models/Dto/Services/CreateServiceDto.cs
@@ -8,7 +8,7 @@ namespace SkincareBookingSystem.Models.Dto.Services
         public string ServiceName { get; set; } = null!;
         public string Description { get; set; } = null!;
 
-        [Range(10000, double.MaxValue, ErrorMessage = "Giá dịch vụ phải từ 10,000 trở lên.")]
+        [Range(10000, double.MinValue, ErrorMessage = "Giá dịch vụ phải từ 10,000 trở lên.")]
         public double Price { get; set; }
         public string? ImageUrl { get; set; }
     }
