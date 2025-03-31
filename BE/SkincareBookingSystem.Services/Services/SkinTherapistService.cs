234d71cc77afc16fc53ef8992b0617c304943e4c
AbandonedDemon <hoanghiepgamer@gmail.com>
2025-04-01 03:10:56 +0700

    Update to SkinTherapist's API: Get today's appointments

--- BE/SkincareBookingSystem.Services/Services/SkinTherapistService.cs
+++ BE/SkincareBookingSystem.Services/Services/SkinTherapistService.cs
@@ -1,14 +1,18 @@
 ﻿using SkincareBookingSystem.DataAccess.IRepositories;
 using SkincareBookingSystem.Models.Domain;
+using SkincareBookingSystem.Models.Dto.Booking.Appointment.FinalizeAppointment;
 using SkincareBookingSystem.Models.Dto.Booking.ServiceType;
 using SkincareBookingSystem.Models.Dto.Response;
 using SkincareBookingSystem.Models.Dto.SkinTherapist;
+using SkincareBookingSystem.Models.Dto.Staff;
 using SkincareBookingSystem.Services.Helpers.Responses;
+using SkincareBookingSystem.Services.Helpers.Users;
 using SkincareBookingSystem.Services.IServices;
 using SkincareBookingSystem.Utilities.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -119,6 +123,71 @@ namespace SkincareBookingSystem.Services.Services
             return ErrorResponse.Build(
                 message: StaticOperationStatus.SkinTherapist.NotFound,
                 statusCode: StaticOperationStatus.StatusCode.NotFound);
+        }
+
+        public async Task<ResponseDto> GetTherapistTodayAppointments(ClaimsPrincipal User)
+        {
+            if (UserError.NotExists(User))
+                return ErrorResponse.Build(
+                    message: StaticResponseMessage.User.NotFound,
+                    statusCode: StaticOperationStatus.StatusCode.NotFound);
+
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var therapistFromDb = await _unitOfWork.SkinTherapist.GetAsync(st => st.UserId == userId);
+
+                if (therapistFromDb is null)
+                    return ErrorResponse.Build(
+                        message: "Insufficient permission!",
+                        statusCode: StaticOperationStatus.StatusCode.BadRequest);
+
+                var today = DateOnly.FromDateTime(DateTime.UtcNow.AddHours(7));
+
+                var appointmentsFromDb = await _unitOfWork.Appointments.GetAllAsync(
+                    filter: a => a.AppointmentDate == today && a.TherapistSchedules.Any(ts => ts.TherapistId == therapistFromDb.SkinTherapistId),
+                    includeProperties: $"{nameof(Appointments.TherapistSchedules)}," +
+                       $"{nameof(Appointments.TherapistSchedules)}.{nameof(TherapistSchedule.SkinTherapist)}," +
+                       $"{nameof(Appointments.TherapistSchedules)}.{nameof(TherapistSchedule.SkinTherapist)}.{nameof(SkinTherapist.ApplicationUser)}," +
+                       $"{nameof(Appointments.Customer)}," +
+                       $"{nameof(Appointments.Customer)}.{nameof(Customer.ApplicationUser)}");
+
+                var appointmentListDto = appointmentsFromDb.Select(a =>
+                {
+                    var activeSchedule = a.TherapistSchedules?.FirstOrDefault
+                    (
+                        ts => ts.ScheduleStatus != ScheduleStatus.Cancelled &&
+                              ts.ScheduleStatus != ScheduleStatus.Rescheduled
+                    );
+
+                    return new GetTodayAppointmentDto
+                    {
+                        Therapist = activeSchedule?.SkinTherapist?.ApplicationUser?.FullName ?? "Not Assigned",
+                        Customer = a.Customer?.ApplicationUser?.FullName ?? "Unknown Customer",
+                        Time = activeSchedule?.Slot?.StartTime.ToString("HH:mm") ?? "Not Scheduled",
+                        Status = activeSchedule?.ScheduleStatus,
+                        AppointmentId = a.AppointmentId,
+                        CustomerId = a.CustomerId
+                    };
+                }).ToList();
+
+                return new ResponseDto()
+                {
+                    Result = new
+                    {
+                        Appointments = appointmentListDto
+                    },
+                    Message = "Today's appointments retrieved successfully",
+                    IsSuccess = true,
+                    StatusCode = 200
+                };
+            }
+            catch (Exception ex)
+            {
+                return ErrorResponse.Build(
+                    message: ex.Message,
+                    statusCode: StaticOperationStatus.StatusCode.InternalServerError);
+            }
         }
     }
 }
